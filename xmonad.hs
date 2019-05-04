728f40f38a3cbd9185430844523d1cd947561185
Karthik Iyengar <k.iyengar@klarna.com>
2019-05-04 16:35:45 +0200

    Polybar, disable greedy view

--- xmonad.hs
+++ xmonad.hs
@@ -9,22 +9,48 @@ import           XMonad.Util.NamedWindows       ( getName )
 import           XMonad.Hooks.DynamicLog
 import qualified XMonad.StackSet               as W
 import           System.IO
+import           XMonad.Hooks.EwmhDesktops      ( ewmh )
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Util.EZConfig
 
-myBar = 
- "polybar xmother"
+myBar = "polybar xmother"
 
-main = do
-  forM_ [".xmonad-workspace-log", ".xmonad-title-log"] $ \file -> do
-    safeSpawn "mkfifo" ["/tmp/" ++ file]
-    xmonad =<< statusBar
-      myBar
-      def
-      def
-      def { terminal = "tilix", modMask = mod4Mask, logHook = eventLogHook, focusedBorderColor = "#008080" }
+myStatusBar = statusBar myBar def def
+
+myMod = mod4Mask
+
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+myKeysP =
+  [ (otherModMasks ++ "M-" ++ [key], action tag)
+  | (tag          , key   ) <- zip myWorkspaces "123456789"
+  , (otherModMasks, action) <-
+    [ ( ""
+      , windows . W.view
+      ) -- was W.greedyView
+    , ("S-", windows . W.shift)
+    ]
+  ]
+
+myKeys =
+  [ ( (myMod, xK_b)
+    , sequence_ [spawn "polybar-msg cmd toggle", sendMessage ToggleStruts]
+    )
+  ]
 
 
+myConfig =
+  def { terminal           = "tilix"
+      , modMask            = mod4Mask
+      , focusedBorderColor = "#008080"
+      , logHook            = myEventLogHook
+      , layoutHook         = avoidStruts $ layoutHook defaultConfig
+      }
+    `additionalKeysP` myKeysP
+    `additionalKeys`  myKeys
 
-eventLogHook = do
+
+myEventLogHook = do
   winset <- gets windowset
   title  <- maybe (return "") (fmap show . getName) . W.peek $ winset
   let currWs = W.currentTag winset
@@ -38,3 +64,13 @@ eventLogHook = do
   fmt currWs ws | currWs == ws = "[" ++ ws ++ "]"
                 | otherwise    = " " ++ ws ++ " "
   sort' = sortBy (compare `on` (!! 0))
+
+
+main = do
+  forM_ [".xmonad-workspace-log", ".xmonad-title-log"] $ \file -> do
+    safeSpawn "mkfifo" ["/tmp/" ++ file]
+  xmonad =<< myStatusBar myConfig
+
+
+
+
