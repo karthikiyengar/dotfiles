261bafef82738895a6967715106276db45debbdd
Karthik Iyengar <k.iyengar@klarna.com>
2020-01-23 16:39:39 +0100

    add fzf

---
+++ fzf/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/kiyengar/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/kiyengar/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/kiyengar/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/kiyengar/.fzf/shell/key-bindings.zsh"
