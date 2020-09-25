0fac9ab1a8862a3c79c8f7b6ca9adec52d361050
Karthik Iyengar <hello@kiyengar.net>
2020-09-25 08:18:08 +0200

    use urxvt

--- xmonad-nix/.xmonad/xmonad.hs
+++ xmonad-nix/.xmonad/xmonad.hs
@@ -28,7 +28,7 @@ import           XMonad.Util.SpawnOnce
 import qualified Data.Map                      as M
 import           XMonad.Actions.CopyWindow      ( copy )
 
-myTerminal = "termonad"
+myTerminal = "urxvtc"
 myMod = mod4Mask -- Super Key
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
