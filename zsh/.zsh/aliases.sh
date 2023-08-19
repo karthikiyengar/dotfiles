137cd98ee5e84b2fb7c15ec347a29f3fb9b62e8f
Karthik Iyengar <hello@kiyengar.net>
2023-08-19 21:52:51 +0200

    Add color to grep and ls

--- zsh/.zsh/aliases.sh
+++ zsh/.zsh/aliases.sh
@@ -4,6 +4,7 @@ alias ga="git add --all"
 alias gc="git commit"
 alias sz="source ~/.zshrc"
 alias vz="vim ~/.dotfiles"
-alias u="sudo apt update && sudo apt upgrade" 
-alias ls="ls -la"
+alias u="sudo apt update && sudo apt upgrade"
+alias ls="ls -la --color=auto"
+alias grep='grep --color'
 
