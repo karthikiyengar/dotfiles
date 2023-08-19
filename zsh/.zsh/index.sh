87bab76e4bb8615e84db17a06904ab27938614ad
Karthik Iyengar <hello@kiyengar.net>
2023-08-19 21:45:30 +0200

    Fix some keybindings

--- zsh/.zsh/index.sh
+++ zsh/.zsh/index.sh
@@ -1,5 +1,7 @@
-source  $HOME/.zsh/prompt.sh
-source  $HOME/.zsh/plugins.sh
-source  $HOME/.zsh/aliases.sh
-source  $HOME/.zsh/completion.sh
-source  $HOME/.zsh/path.sh
+source $HOME/.zsh/prompt.sh
+source $HOME/.zsh/plugins.sh
+source $HOME/.zsh/history.sh
+source $HOME/.zsh/aliases.sh
+source $HOME/.zsh/completion.sh
+source $HOME/.zsh/navigation.sh
+source $HOME/.zsh/path.sh
---
+++ zsh/.zsh/navigation.sh
@@ -0,0 +1,9 @@
+setopt autocd
+
+bindkey "^[[1;5C" forward-word # Ctrl + Right
+bindkey "^[[1;5D" backward-word # Ctrl + Left
+bindkey  "^[[H"   beginning-of-line
+bindkey  "^[[F"   end-of-line
+bindkey  "^[[3~"  delete-char
+bindkey '^H' backward-kill-word
+bindkey '5~' kill-word
