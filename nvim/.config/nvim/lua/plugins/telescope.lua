b525e0106454d68cfaa5ab56bc1c4c466a85e696
Karthik Iyengar <hello@kiyengar.net>
2023-10-03 14:27:12 +0200

    Make telescope work with hidden files reliably

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,22 @@ return {
     keys = {
       { "<leader>,", false },
     },
+    defaults = {
+      vimgrep_arguments = {
+        "rg",
+        "--hidden",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+      },
+    },
     pickers = {
+      find_files = {
+        hidden = true,
+      },
       live_grep = {
         find_command = { "rg", "--hidden" },
       },
