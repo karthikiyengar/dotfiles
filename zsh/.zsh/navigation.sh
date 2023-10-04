cdc2bcc71ef39f28b2c27ba52fc14746589fd667
Karthik Iyengar <hello@kiyengar.net>
2023-10-04 16:29:35 +0200

    Before swaymonad

--- zsh/.zsh/navigation.sh
+++ zsh/.zsh/navigation.sh
@@ -1,12 +1,15 @@
+# I use Ctrl + D in Vim to scroll. Don't want to kill the terminal accidentally.
+set -o ignoreeof
+
 export VISUAL=nvim
 export EDITOR="$VISUAL"
 
 setopt autocd
 
-bindkey "^[[1;5C" forward-word # Ctrl + Right
+bindkey "^[[1;5C" forward-word  # Ctrl + Right
 bindkey "^[[1;5D" backward-word # Ctrl + Left
-bindkey  "^[[H"   beginning-of-line
-bindkey  "^[[F"   end-of-line
-bindkey  "^[[3~"  delete-char
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char
 bindkey '^H' backward-kill-word
 bindkey '5~' kill-word
