2d7516d22e2943623e34954e45c038b1406eeea9
Karthik Iyengar <hello@kiyengar.net>
2024-04-16 22:40:07 +0200

    Source cargo only if exists

--- zsh/.zsh/dev.sh
+++ zsh/.zsh/dev.sh
@@ -2,7 +2,9 @@
 [ -f ~/.rbenv/bin/rbenv ] && eval "$(~/.rbenv/bin/rbenv init - zsh)"
 
 # Rust/Cargo
-source "$HOME/.cargo/env"
+if [ -f "$HOME/.cargo/env" ]; then
+    source "$HOME/.cargo/env"
+fi
 
 # Add ssh key to agent
 command -v keychain >/dev/null 2>&1 && eval $(keychain -q --eval id_rsa)
