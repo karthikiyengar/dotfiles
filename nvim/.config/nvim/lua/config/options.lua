063c9d7cb7729ddfa7702c0ea30581e6636966ae
Karthik Iyengar <hello@kiyengar.net>
2024-07-22 23:03:40 +0000

    Set default clipboard for vim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 vim.g.suda_smart_edit = 1
 vim.g.blamer_enabled = true
 vim.opt.title = true
